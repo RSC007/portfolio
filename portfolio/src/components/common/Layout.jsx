454618e245c7abf8a06eaa59a33d8e7f25463e7c
Rushikesh <rushikesh.chitte@cp-360.com>
2024-01-07 01:48:35 +0530

    layout build issue fix

--- portfolio/src/components/common/Layout.jsx
+++ portfolio/src/components/common/Layout.jsx
@@ -1,31 +1,8 @@
-import { useState } from "react";
-import {
-  Box,
-  Drawer,
-  CssBaseline,
-  AppBar,
-  Toolbar,
-  IconButton,
-  Typography,
-} from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
 import { Outlet } from "react-router-dom";
 
 import SideBar from "./SideBar";
 
-const drawerWidth = 300;
-
-export default function Layout({ ...props }) {
-  const { window: wd } = props;
-
-  const container = wd !== undefined ? () => wd().document.body : undefined;
-
-  const [mobileOpen, setMobileOpen] = useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
-
+export default function Layout() {
   return (
     <>
       <SideBar>
